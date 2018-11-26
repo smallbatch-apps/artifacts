01d3b4fd4b8fe15bb83f5988375adff3661e295e
Matt Burgess <matt@smallbatch.io>
2018-11-27 01:51:31 +1000

    Add index file to allow direct exporting of functions

---
+++ index.js
@@ -0,0 +1,25 @@
+const AUD = require('contracts/AUD');
+const ERC20 = require('contracts/ERC20');
+const OrderBook = require('contracts/OrderBook');
+const Roles = require('contracts/Roles');
+const Stats = require('contracts/Stats');
+const TradingZones = require('contracts/TradingZones');
+const TradingPeriodRestriction = require('contracts/TradingPeriodRestriction');
+const Users = require('contracts/Users');
+const VolumeRestriction = require('contracts/VolumeRestriction');
+const Water = require('contracts/Water');
+const WaterLicense = require('contracts/WaterLicense');
+
+module.exports = {
+  AUD,
+  ERC20,
+  OrderBook,
+  Roles,
+  Stats,
+  TradingZones,
+  TradingPeriodRestriction,
+  Users,
+  VolumeRestriction,
+  Water,
+  WaterLicense
+}
