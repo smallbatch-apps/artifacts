a0cf32aa30fec263d45db9952b6afdc443b1bf12
Matt Burgess <matt@smallbatch.io>
2018-11-27 12:05:41 +1000

    correct local paths

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const AUD = require('contracts/AUD');
-const ERC20 = require('contracts/ERC20');
-const OrderBook = require('contracts/OrderBook');
-const Roles = require('contracts/Roles');
-const Stats = require('contracts/Stats');
-const TradingZones = require('contracts/TradingZones');
-const TradingPeriodRestriction = require('contracts/TradingPeriodRestriction');
-const Users = require('contracts/Users');
-const VolumeRestriction = require('contracts/VolumeRestriction');
-const Water = require('contracts/Water');
-const WaterLicense = require('contracts/WaterLicense');
+const AUD = require('./contracts/AUD');
+const ERC20 = require('./contracts/ERC20');
+const OrderBook = require('./contracts/OrderBook');
+const Roles = require('./contracts/Roles');
+const Stats = require('./contracts/Stats');
+const TradingZones = require('./contracts/TradingZones');
+const TradingPeriodRestriction = require('./contracts/TradingPeriodRestriction');
+const Users = require('./contracts/Users');
+const VolumeRestriction = require('./contracts/VolumeRestriction');
+const Water = require('./contracts/Water');
+const WaterLicense = require('./contracts/WaterLicense');
 
 module.exports = {
   AUD,
